049e8b1d2ab3557a746f698e5cbe1b2cef40f8e3
Samara Dias da Silva <marcoscz329@gmail.com>
2026-03-09 12:26:45 -0300

    Exercicio simples

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int valor = 7;
+        if(valor % 2 == 0) {
+            System.out.println("O número " + valor + " é par.");
+        } else {
+            System.out.println("O número " + valor + " é impar.");
+        }
+
+    }
+}
